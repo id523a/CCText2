84fd2a31b7e670169878cb75e40624f82be73e7f
Edward Giles <edward@giles.net.au>
2020-01-05 12:14:24 +0800

    Added scripts

---
+++ tape.lua
@@ -0,0 +1,92 @@
+function printHelp()
+  print("tape label (<side>) <label>")
+  print("tape play (<side>)")
+  print("tape rewind (<side>)")
+  print("tape setspeed (<side>) <value>")
+  print("tape stop (<side>)")
+end
+
+function getTapeSide()
+  local periphList = peripheral.getNames()
+  local tapeList = {}
+  for k,v in pairs(periphList) do
+    if (peripheral.getType(v) == "tape_drive") then
+      table.insert(tapeList, v)
+    end
+  end
+  if (#tapeList == 0) then
+    print("No tape-drive attached")
+    return nil
+  end
+  if (#tapeList == 1) then
+    return tapeList[1]
+  else
+    print(table.concat(tapeList, ","))
+    print("Select side:")
+    return read()
+  end
+end
+
+local args = {...}
+if (#args < 1) then
+  printHelp()
+  return
+end
+local cmd = table.remove(args, 1)
+local selectSide = nil
+local expectedArgs = {
+  ["label"] = 1,
+  ["play"] = 0,
+  ["rewind"] = 0,
+  ["setspeed"] = 1,
+  ["stop"] = 0
+}
+expectedArgs = expectedArgs[cmd]
+if (expectedArgs == nil) then
+  print("Invalid command")
+  return
+end
+if (#args < expectedArgs) then
+  print("Too few arguments")
+  return
+end
+if (#args > expectedArgs) then
+  selectSide = table.remove(args, 1)
+else
+  selectSide = getTapeSide()
+end
+if (peripheral.getType(selectSide) ~= "tape_drive") then
+  print("Invalid side")
+  return
+end
+local tape = peripheral.wrap(selectSide)
+if (not tape.isReady()) then
+  print("No tape inserted")
+  return
+end
+if (cmd == "label") then
+  tape.setLabel(args[1])
+elseif (cmd == "play") then
+  tape.seek(-tape.getSize())
+  local speed = (tape.read() / 255) + 1
+  tape.setSpeed(speed)
+  tape.play()
+elseif (cmd == "rewind" or cmd == "stop") then
+  tape.stop()
+  tape.seek(-tape.getSize())
+elseif (cmd == "setspeed") then
+  local newSpeed = tonumber(args[1])
+  if (newSpeed == nil) then
+    print("Speed must be a number.")
+	return
+  elseif (newSpeed >= 32768 and newSpeed <= 65536) then
+    newSpeed = newSpeed / 32768
+  elseif (newSpeed < 1.0 or newSpeed > 2.0) then
+    print("Speed must be between 1 and 2 (multiplier)")
+	print("or between 32768 and 65536 (samplerate).")
+	return
+  end
+  tape.stop()
+  tape.seek(-tape.getSize())
+  tape.write(math.floor((newSpeed - 1) * 255 + 0.5))
+end
---
+++ writetape_b64.lua
@@ -0,0 +1,116 @@
+local byte_A = string.byte("A")
+local byte_Z = string.byte("Z")
+local byte_a = string.byte("a")
+local byte_z = string.byte("z")
+local byte_0 = string.byte("0")
+local byte_9 = string.byte("9")
+local byte_62 = string.byte("+")
+local byte_63 = string.byte("/")
+
+function base64_decode(str, byteCallback)
+  local stringLength = string.len(str)
+  local bits = 0
+  local bitCount = 0
+  -- For each byte in string
+  for i = 1,stringLength do
+    local strByte = string.byte(str, i)
+    local b64Digit = -1
+    -- Determine its digit value in base64
+    if (strByte == byte_62) then
+      b64Digit = 62
+    elseif (strByte == byte_63) then
+      b64Digit = 63
+    elseif (strByte >= byte_A and strByte <= byte_Z) then
+      b64Digit = strByte - byte_A
+    elseif (strByte >= byte_a and strByte <= byte_z) then
+      b64Digit = strByte - byte_a + 26
+    elseif (strByte >= byte_0 and strByte <= byte_9) then
+      b64Digit = strByte - byte_0 + 52
+    end
+    -- If it's a valid digit, shift it onto the output
+    if (b64Digit >= 0) then
+      bits = bit32.lshift(bits, 6)
+      bits = bit32.bor(bits, b64Digit)
+      bitCount = bitCount + 6
+      -- If there are enough bits for a byte, shift it out
+      if (bitCount >= 8) then
+        bitCount = bitCount - 8
+        local byteVal = bit32.rshift(bits, bitCount)
+        bits = bit32.band(bits, bit32.lshift(1, bitCount) - 1)
+        byteCallback(byteVal)
+      end
+    end
+  end
+end
+
+local args = {...}
+local filename = args[1]
+if (filename == nil or filename == "") then
+  print("writetape (<side>) <file>")
+  return
+end
+local selectSide = nil
+if (#args >= 2) then
+  selectSide = args[1]
+  filename = args[2]
+end
+if (selectSide == nil) then
+  local periphList = peripheral.getNames()
+  local tapeList = {}
+  for k,v in pairs(periphList) do
+    if (peripheral.getType(v) == "tape_drive") then
+      table.insert(tapeList, v)
+    end
+  end
+  if (#tapeList == 0) then
+    error("No tape-drive attached")
+  end
+  if (#tapeList == 1) then
+    selectSide = tapeList[1]
+  else
+    print(table.concat(tapeList, ","))
+    print("Select side:")
+    selectSide = read()
+  end
+end
+
+if (peripheral.getType(selectSide) ~= "tape_drive") then
+  error("Invalid side")
+end
+local tape = peripheral.wrap(selectSide)
+if (not tape.isReady()) then
+  error("No tape inserted")
+end
+
+local file = nil
+local fileIsHTTP = false
+if (string.find(filename, "://")) then
+  fileIsHTTP = true
+  print("Requesting...")
+  file = http.get(filename)
+else
+  file = fs.open(filename, "r")
+end
+if (not file) then
+  error("Unable to open " .. filename)
+end
+
+if (fileIsHTTP) then
+  local responseCode = file.getResponseCode()
+  if (responseCode >= 400 and responseCode <= 600) then
+    file.close()
+    error("HTTP error: " .. responseCode)
+  end
+end
+
+local ctr = 0
+base64_decode(file.readAll(), function(byteVal)
+  tape.write(byteVal)
+  ctr = ctr + 1
+  if (ctr >= 65536) then
+    ctr = 0
+    sleep(0.1)
+    print("Written ", i, " bytes")
+  end
+end)
+file.close()
